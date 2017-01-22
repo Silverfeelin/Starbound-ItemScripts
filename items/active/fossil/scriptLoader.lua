daefd2535df93c51eea7d957416b822a5572e9ce
Silverfeelin <Silverfeelin@gmail.com>
2017-01-22 21:56:07 +0100

    Initial version
    Not fully tested yet. Compatibility added for my ItemInterfaces mod.

---
+++ items/active/fossil/scriptLoader.lua
@@ -0,0 +1,31 @@
+local scriptPath = config.getParameter("itemScript") and config.getParameter("itemScript.script")
+
+-- Compatibility check for Item Interfaces v1.0.0(+)
+if not scriptPath then
+  if config.getParameter("itemInterface") then
+    scriptPath = "/items/active/fossil/activeItemInterface.lua"
+  else
+    return
+  end
+end
+
+-- Store references
+local i, up, un = init, update, uninit
+
+-- Check if the file exists.
+if not pcall(function() require(scriptPath) end) then
+  sb.logError("ItemScript: Could not load the script '%s'.", scriptPath)
+  return
+end
+
+-- New init is assigned, but init is already called to reach this code. To fix, we call it manually.
+if i ~= init then init() end
+
+-- New update isn't assigned; disable the update callback.
+if up == update then update = nil end
+
+-- New uninit isn't assigned; disable the uninit callback.
+if un == uninit then uninit = nil end
+
+-- If we reached this part, that means a valid script was loaded.
+itemScript = true
